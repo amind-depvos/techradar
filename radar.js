db585220be49edb7eb4667aa446db72fcd04a937
Amind Depvos <amind.depvos.mjukutv@sweet-salt.com>
2016-07-08 13:41:45 +0200

    Taking fixes for outer layer fixes
    See pull request for more info https://github.com/bdargan/techradar/pull/9

--- radar.js
+++ radar.js
@@ -100,12 +100,7 @@ for (var i = 0; i < radar_data.length; i++) {
     });
     
     var offsetIndex = 0;
-    for (var stageIdx in _(itemsByStage).keys()) {
-
-        if (stageIdx > 0) {
-            offsetIndex = offsetIndex + itemsByStage[stageIdx-1].length + 1; 
-            console.log("offsetIndex = " + itemsByStage[stageIdx-1].length, offsetIndex );
-        }
+    for (var stageIdx in itemsByStage) {
 
         radar.add(pv.Label)         
             .left( radar_data[i].left + headingFontSize )
@@ -156,7 +151,9 @@ for (var i = 0; i < radar_data.length; i++) {
           .textBaseline("middle")
           .textStyle("white");            
 
-
+	var offsetterLength = itemsByStage[stageIdx].length;
+	offsetIndex = offsetIndex + offsetterLength + 1; 
+	console.log("offsetIndex = " + offsetterLength, offsetIndex );	
     }
 }      
        
